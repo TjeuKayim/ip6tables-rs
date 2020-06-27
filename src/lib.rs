1c35a78db7a68e3d13dff6db6dfc759eb41c1f53
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-06-27 22:31:56 +0200

    Table6 with ip6tc_init

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
+mod error;
+mod rule;
+
+use libip6tc_sys as sys;
+use std::ffi::{CStr, CString};
+
+use error::IptcError;
+
+struct Table4 {
+    // TODO:
+}
+
+struct Table6 {
+    handle: *mut sys::xtc_handle,
+    table_name: CString,
+}
+
+impl Table6 {
+    pub fn new(table_name: &str) -> Result<Self, IptcError> {
+        let table_name = CString::new(table_name).unwrap();
+        // Takes a snapshot of the rules
+        // https://www.tldp.org/HOWTO/Querying-libiptc-HOWTO/qfunction.html
+        let handle = unsafe { sys::ip6tc_init(table_name.as_ptr()) };
+        if handle.is_null() {
+            Err(IptcError::from_errno())
+        } else {
+            Ok(Self { handle, table_name })
+        }
+    }
+}
+
+struct Chain4 {}
+
+struct Chain6 {}
+
+trait Chain: Sized {
+    fn new(table_name: &str, chain_name: &str) -> Result<Self, IptcError>;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        // let c = Chain6::new("mangle", "TEST");
     }
 }
