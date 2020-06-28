c3857e99361fb8eb9bcd1d89c54fa45c0ff68cf1
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-06-28 13:35:06 +0200

    cargo fmt

--- src/rule.rs
+++ src/rule.rs
@@ -1,12 +1,12 @@
 use libip6tc_sys as sys;
-use std::ffi::{CString};
-use std::mem::{size_of, size_of_val, forget};
+use std::ffi::CString;
+use std::mem::{forget, size_of, size_of_val};
 use std::net::Ipv6Addr;
 // use std::os::raw::c_int;
-use std::alloc::{Layout, Global, AllocRef, AllocInit, ReallocPlacement, handle_alloc_error};
+use std::alloc::{handle_alloc_error, AllocInit, AllocRef, Global, Layout, ReallocPlacement};
+use std::fmt;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
-use std::fmt;
 
 const ALIGN: usize = size_of::<u64>();
 
@@ -55,7 +55,7 @@ impl RuleBuilder<Ipv6Addr> {
         RuleBuilder::new()
     }
 
-    fn src(mut self, ip: Ipv6Addr) -> Self {
+    fn src(self, ip: Ipv6Addr) -> Self {
         let mut entry = self.buf.ptr.cast::<sys::ip6t_entry>();
         let entry = unsafe { entry.as_mut() };
         entry.ipv6.src.__in6_u.__u6_addr16 = ip.segments();
@@ -70,15 +70,16 @@ impl<E: Entry> RuleBuilder<E> {
         debug_assert_eq!(size_of::<E::Sys>() % ALIGN, 0);
         // let ptr = unsafe { alloc_zeroed(Layout::from_size_align(cap, ALIGN).unwrap()) };
         let layout = Layout::from_size_align(cap, ALIGN).unwrap();
-        let block = Global.alloc(layout, AllocInit::Zeroed)
+        let block = Global
+            .alloc(layout, AllocInit::Zeroed)
             .unwrap_or_else(|_| handle_alloc_error(layout));
         RuleBuilder {
-            buf: RuleBuf  {
+            buf: RuleBuf {
                 ptr: block.ptr,
                 cap: block.size,
                 phantom: PhantomData,
             },
-        len: size_of::<sys::ip6t_entry>(),
+            len: size_of::<sys::ip6t_entry>(),
         }
     }
 
@@ -98,12 +99,16 @@ impl<E: Entry> RuleBuilder<E> {
             let new_cap = 2 * self.buf.cap;
             let layout = Layout::from_size_align(self.buf.cap, ALIGN).unwrap();
             dbg!(self.buf.cap, self.len, &layout);
-            let block = unsafe { Global.grow(self.buf.ptr, 
-                layout,
-                new_cap,
-                ReallocPlacement::MayMove,
-                AllocInit::Zeroed
-                ) }.unwrap_or_else(|_| handle_alloc_error(layout));
+            let block = unsafe {
+                Global.grow(
+                    self.buf.ptr,
+                    layout,
+                    new_cap,
+                    ReallocPlacement::MayMove,
+                    AllocInit::Zeroed,
+                )
+            }
+            .unwrap_or_else(|_| handle_alloc_error(layout));
             self.buf.ptr = block.ptr;
             self.buf.cap = block.size;
             dbg!(self.buf.ptr, self.buf.cap);
@@ -119,7 +124,7 @@ impl<E: Entry> RuleBuilder<E> {
         let mut comment = [0i8; MAX];
         cast_signed(&mut comment[0..comment_c.as_bytes().len()])
             .copy_from_slice(comment_c.as_bytes());
-        
+
         let name_c = CString::new("comment").unwrap();
         let mut name = [0i8; 29];
         cast_signed(&mut name[0..name_c.as_bytes().len()]).copy_from_slice(name_c.as_bytes());
@@ -138,7 +143,7 @@ impl<E: Entry> RuleBuilder<E> {
         let data = self.extend::<sys::xt_standard_target>();
         data.verdict = sys::NF_ACCEPT as _;
         let rule = Rule {
-            buf: self.buf.clone()
+            buf: self.buf.clone(),
         };
         forget(self);
         rule
